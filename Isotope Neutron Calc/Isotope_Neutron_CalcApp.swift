633676c73b15b04fadda3b377a69b81b944e9515
Omar Ahmed <developeromi@icloud.com>
2025-05-30 23:48:49 -0400

    Initial Commit

---
+++ Isotope Neutron Calc/Isotope_Neutron_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Isotope_Neutron_CalcApp.swift
+//  Isotope Neutron Calc
+//
+//  Created by Omar Ahmed on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Isotope_Neutron_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
